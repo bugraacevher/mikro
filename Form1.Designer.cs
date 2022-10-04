44da7bc23fc3e826ece12bad810fedf1205b5c69
Buğra Cevher <99588506+bugraacevher@users.noreply.github.com>
2022-10-04 14:32:13 +0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace mikro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGirisRECno = new System.Windows.Forms.TextBox();
+            this.txtGirisSpecial1 = new System.Windows.Forms.TextBox();
+            this.txtGirisDBCno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.txtGirisSpecial2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtGirisSpecial3 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.buttonEkle = new System.Windows.Forms.Button();
+            this.buttonSil = new System.Windows.Forms.Button();
+            this.txtGirisAlti = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtGirisEvrakTip = new System.Windows.Forms.TextBox();
+            this.txtGirisEvraknoSeri = new System.Windows.Forms.TextBox();
+            this.txtGirisEvraknoSira = new System.Windows.Forms.TextBox();
+            this.txtGirisSatirno = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.buttonGuncelle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtGirisRECno
+            // 
+            this.txtGirisRECno.Location = new System.Drawing.Point(138, 65);
+            this.txtGirisRECno.Name = "txtGirisRECno";
+            this.txtGirisRECno.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisRECno.TabIndex = 1;
+            this.txtGirisRECno.Text = "Sayı Giriniz";
+            // 
+            // txtGirisSpecial1
+            // 
+            this.txtGirisSpecial1.Location = new System.Drawing.Point(138, 91);
+            this.txtGirisSpecial1.Name = "txtGirisSpecial1";
+            this.txtGirisSpecial1.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisSpecial1.TabIndex = 2;
+            this.txtGirisSpecial1.Text = "Sayı Giriniz";
+            // 
+            // txtGirisDBCno
+            // 
+            this.txtGirisDBCno.Location = new System.Drawing.Point(138, 39);
+            this.txtGirisDBCno.Name = "txtGirisDBCno";
+            this.txtGirisDBCno.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisDBCno.TabIndex = 0;
+            this.txtGirisDBCno.Text = "Sayı Giriniz";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 120);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "sth_special2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "sth_special1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(62, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "sth_special3";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 176);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(482, 130);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.CellEnter += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEnter);
+            // 
+            // txtGirisSpecial2
+            // 
+            this.txtGirisSpecial2.Location = new System.Drawing.Point(138, 117);
+            this.txtGirisSpecial2.Name = "txtGirisSpecial2";
+            this.txtGirisSpecial2.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisSpecial2.TabIndex = 3;
+            this.txtGirisSpecial2.Text = "Sayı Giriniz";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 42);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "sth_RECid_DBCno";
+            // 
+            // txtGirisSpecial3
+            // 
+            this.txtGirisSpecial3.Location = new System.Drawing.Point(138, 143);
+            this.txtGirisSpecial3.Name = "txtGirisSpecial3";
+            this.txtGirisSpecial3.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisSpecial3.TabIndex = 4;
+            this.txtGirisSpecial3.Text = "Sayı Giriniz";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(31, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(97, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "sth_RECid_RECno";
+            // 
+            // buttonEkle
+            // 
+            this.buttonEkle.Location = new System.Drawing.Point(12, 312);
+            this.buttonEkle.Name = "buttonEkle";
+            this.buttonEkle.Size = new System.Drawing.Size(119, 32);
+            this.buttonEkle.TabIndex = 5;
+            this.buttonEkle.Text = "EKLE";
+            this.buttonEkle.UseVisualStyleBackColor = true;
+            this.buttonEkle.Click += new System.EventHandler(this.buttonEkle_Click);
+            // 
+            // buttonSil
+            // 
+            this.buttonSil.Location = new System.Drawing.Point(205, 312);
+            this.buttonSil.Name = "buttonSil";
+            this.buttonSil.Size = new System.Drawing.Size(119, 32);
+            this.buttonSil.TabIndex = 6;
+            this.buttonSil.Text = "SIL";
+            this.buttonSil.UseVisualStyleBackColor = true;
+//            this.buttonSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // txtGirisAlti
+            // 
+            this.txtGirisAlti.Enabled = false;
+            this.txtGirisAlti.Location = new System.Drawing.Point(138, 13);
+            this.txtGirisAlti.Name = "txtGirisAlti";
+            this.txtGirisAlti.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisAlti.TabIndex = 9;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(62, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 13);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "sth_RECno";
+            // 
+            // txtGirisEvrakTip
+            // 
+            this.txtGirisEvrakTip.Location = new System.Drawing.Point(366, 39);
+            this.txtGirisEvrakTip.Name = "txtGirisEvrakTip";
+            this.txtGirisEvrakTip.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisEvrakTip.TabIndex = 1;
+            this.txtGirisEvrakTip.Text = "Sayı Giriniz";
+            // 
+            // txtGirisEvraknoSeri
+            // 
+            this.txtGirisEvraknoSeri.Location = new System.Drawing.Point(366, 65);
+            this.txtGirisEvraknoSeri.Name = "txtGirisEvraknoSeri";
+            this.txtGirisEvraknoSeri.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisEvraknoSeri.TabIndex = 2;
+            this.txtGirisEvraknoSeri.Text = "Sayı Giriniz";
+            // 
+            // txtGirisEvraknoSira
+            // 
+            this.txtGirisEvraknoSira.Location = new System.Drawing.Point(366, 91);
+            this.txtGirisEvraknoSira.Name = "txtGirisEvraknoSira";
+            this.txtGirisEvraknoSira.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisEvraknoSira.TabIndex = 3;
+            this.txtGirisEvraknoSira.Text = "Sayı Giriniz";
+            // 
+            // txtGirisSatirno
+            // 
+            this.txtGirisSatirno.Location = new System.Drawing.Point(366, 117);
+            this.txtGirisSatirno.Name = "txtGirisSatirno";
+            this.txtGirisSatirno.Size = new System.Drawing.Size(113, 20);
+            this.txtGirisSatirno.TabIndex = 4;
+            this.txtGirisSatirno.Text = "Sayı Giriniz";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(267, 94);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(88, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "sth_evrakno_sira";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(267, 68);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(88, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "sth_evrakno_seri";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(297, 120);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(58, 13);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "sth_satirno";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(290, 42);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(65, 13);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "sth_evraktip";
+            // 
+            // buttonGuncelle
+            // 
+            this.buttonGuncelle.Location = new System.Drawing.Point(375, 312);
+            this.buttonGuncelle.Name = "buttonGuncelle";
+            this.buttonGuncelle.Size = new System.Drawing.Size(119, 32);
+            this.buttonGuncelle.TabIndex = 11;
+            this.buttonGuncelle.Text = "GUNCELLE";
+            this.buttonGuncelle.UseVisualStyleBackColor = true;
+            this.buttonGuncelle.Click += new System.EventHandler(this.buttonGuncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(501, 362);
+            this.Controls.Add(this.buttonGuncelle);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtGirisAlti);
+            this.Controls.Add(this.buttonSil);
+            this.Controls.Add(this.buttonEkle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGirisDBCno);
+            this.Controls.Add(this.txtGirisSatirno);
+            this.Controls.Add(this.txtGirisEvraknoSira);
+            this.Controls.Add(this.txtGirisSpecial3);
+            this.Controls.Add(this.txtGirisEvraknoSeri);
+            this.Controls.Add(this.txtGirisSpecial2);
+            this.Controls.Add(this.txtGirisEvrakTip);
+            this.Controls.Add(this.txtGirisSpecial1);
+            this.Controls.Add(this.txtGirisRECno);
+            this.Name = "Form1";
+            this.Text = "DENEME";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGirisRECno;
+        private System.Windows.Forms.TextBox txtGirisSpecial1;
+        private System.Windows.Forms.TextBox txtGirisDBCno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtGirisSpecial2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtGirisSpecial3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button buttonEkle;
+        private System.Windows.Forms.Button buttonSil;
+        private System.Windows.Forms.TextBox txtGirisAlti;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtGirisEvrakTip;
+        private System.Windows.Forms.TextBox txtGirisEvraknoSeri;
+        private System.Windows.Forms.TextBox txtGirisEvraknoSira;
+        private System.Windows.Forms.TextBox txtGirisSatirno;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button buttonGuncelle;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace mikro
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti;
+        SqlCommand komut;
+        SqlDataAdapter da;
+        DataTable tablo;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void VeriIzleme()
+            //server ile baglanti sagladik
+        {   baglanti = new SqlConnection("SERVER=DESKTOP-8LLIB44\\USERB;Initial Catalog=MikroDB_V15_TEST;Integrated Security =SSPI");
+            baglanti.Open();
+            //sutun basliklarini select ile from arasinda cagirdik/
+            da = new SqlDataAdapter("SELECT sth_RECno,sth_RECid_DBCno, sth_RECid_RECno,sth_special1,sth_special2,sth_special3, sth_evraktip, sth_evrakno_seri, sth_evrakno_sira,sth_satirno FROM STOK_HAREKETLERI", baglanti);    
+            tablo = new DataTable();  
+            da.Fill(tablo);
+            dataGridView1.DataSource = tablo;
+            baglanti.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            VeriIzleme();
+        }
+
+        private void dataGridView1_CellEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            //secili satirdaki degerleri textboxlarin icerisine atar bu sayede guncelleme ve silme islemleri daha rahat halledilir.
+                txtGirisAlti.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                txtGirisDBCno.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+                txtGirisRECno.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+                txtGirisSpecial1.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+                txtGirisSpecial2.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+                txtGirisSpecial3.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+                txtGirisEvrakTip.Text = dataGridView1.CurrentRow.Cells[6].Value.ToString();
+                txtGirisEvraknoSeri.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+                txtGirisEvraknoSira.Text = dataGridView1.CurrentRow.Cells[8].Value.ToString();
+                txtGirisSatirno.Text = dataGridView1.CurrentRow.Cells[9].Value.ToString();
+            
+        }
+
+        private void buttonEkle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "INSERT INTO STOK_HAREKETLERI (sth_RECid_DBCno, sth_RECid_RECno, sth_special1, sth_special2, sth_special3, sth_evraktip, sth_evrakno_seri, sth_evrakno_sira, sth_satirno) VALUES (@sth_RECid_DBCno, @sth_RECid_RECno, @sth_special1, @sth_special2, @sth_special3, @sth_evraktip, @sth_evrakno_seri, @sth_evrakno_sira, @sth_satirno)";
+            
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@sth_RECid_DBCno", txtGirisDBCno.Text);
+            komut.Parameters.AddWithValue("@sth_RECid_RECno", txtGirisRECno.Text);
+            komut.Parameters.AddWithValue("@sth_special1", txtGirisSpecial1.Text);
+            komut.Parameters.AddWithValue("@sth_special2", txtGirisSpecial2.Text);
+            komut.Parameters.AddWithValue("@sth_special3", txtGirisSpecial3.Text);
+            komut.Parameters.AddWithValue("@sth_evraktip",txtGirisEvrakTip.Text);
+            komut.Parameters.AddWithValue("@sth_evrakno_seri", txtGirisEvraknoSeri.Text);
+            komut.Parameters.AddWithValue("@sth_evrakno_sira", txtGirisEvraknoSira.Text);
+            komut.Parameters.AddWithValue("@sth_satirno", txtGirisSatirno.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            VeriIzleme();
+        }
+
+        private void buttonSil_Click(object sender, EventArgs e)
+        {
+            string sorgu = "DELETE FROM STOK_HAREKETLERI where sth_RECno=@sth_RECno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@sth_RECno",Convert.ToInt32(txtGirisAlti.Text));
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            VeriIzleme();
+        }
+
+        private void buttonGuncelle_Click(object sender, EventArgs e)
+        {
+            string sorgu = "UPDATE STOK_HAREKETLERI SET sth_RECid_DBCno=@sth_RECid_DBCno, sth_RECid_RECno=@sth_RECid_RECno, sth_special1=@sth_special1, sth_special2=@sth_special2, sth_special3=@sth_special3, sth_evraktip=@sth_evraktip, sth_evrakno_seri=@sth_evrakno_seri, sth_evrakno_sira=@sth_evrakno_sira, sth_satirno=@sth_satirno  WHERE sth_RECno=@sth_RECno";
+            komut = new SqlCommand(sorgu, baglanti);
+            komut.Parameters.AddWithValue("@sth_RECno", txtGirisAlti.Text);
+            komut.Parameters.AddWithValue("@sth_RECid_DBCno", txtGirisDBCno.Text);
+            komut.Parameters.AddWithValue("@sth_RECid_RECno", txtGirisRECno.Text);
+            komut.Parameters.AddWithValue("@sth_special1", txtGirisSpecial1.Text);
+            komut.Parameters.AddWithValue("@sth_special2", txtGirisSpecial2.Text);
+            komut.Parameters.AddWithValue("@sth_special3", txtGirisSpecial3.Text);
+            komut.Parameters.AddWithValue("@sth_evraktip", txtGirisEvrakTip.Text);
+            komut.Parameters.AddWithValue("@sth_evrakno_seri", txtGirisEvraknoSeri.Text);
+            komut.Parameters.AddWithValue("@sth_evrakno_sira", txtGirisEvraknoSira.Text);
+            komut.Parameters.AddWithValue("@sth_satirno", txtGirisSatirno.Text);
+            baglanti.Open();
+            komut.ExecuteNonQuery();
+            baglanti.Close();
+            VeriIzleme();
+        }
+    }
+}
